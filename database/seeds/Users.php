1af6bf9fd709ab6391fc36f9d630a6daca9e963c
Steve Lefevre <lefevre.10@osu.edu>
2020-05-16 21:56:11 -0400

    fleshed out users table

--- database/seeds/Users.php
+++ database/seeds/Users.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class Users extends Seeder
 {
@@ -11,6 +12,23 @@ class Users extends Seeder
      */
     public function run()
     {
-        //
+        $faker = \Faker\Factory::create();
+
+        // librarian is user id 1
+        User::create([
+            'name' => 'librarian',
+            'email' => 'librarian@test.com',
+            'password' => Hash::make(Str::random(20)),
+        ]);
+
+        // And now let's generate a few dozen users for our app:
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => Hash::make(Str::random(20)),
+            ]);
+        }
+
     }
 }
