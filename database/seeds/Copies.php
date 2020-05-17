6755beb236dad72bd24058edc857246577b31dfc
Steve Lefevre <lefevre.10@osu.edu>
2020-05-16 21:47:01 -0400

    stubbed seeders

---
+++ database/seeds/Copies.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Copies extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
