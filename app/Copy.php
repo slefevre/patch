49584171be3a85ac3277f3191a3037385186bc22
Steve Lefevre <lefevre.10@osu.edu>
2020-05-17 20:45:26 -0400

    Added methods for checkout

--- app/Copy.php
+++ app/Copy.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Copy extends Model
 {
-    //
+    public function checkout($user_id, $sn) {
+
+    }
+
+    public function return($user_id, $sn) {
+
+    }
+
+    public function checkedOut($user_id) {
+
+    }
 }
