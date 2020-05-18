131888303f192a828b5f915372935108f93bcc15
Steve Lefevre <lefevre.10@osu.edu>
2020-05-17 20:36:02 -0400

    Added methods to titles model

--- app/Title.php
+++ app/Title.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Title extends Model
 {
-    //
+    public function add($title, $isbn, $media) {
+    }
+
+    public function remove($isbn) {
+    }
+
+    public function overdue() {
+    }
+
+    public function list() {
+    }
+
 }
