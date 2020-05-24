53cd14ad4033f630218c022a3b09d724dcce9412
Steve Lefevre <lefevre.10@osu.edu>
2020-05-23 21:42:48 -0400

    Stubbed out all endpoints

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,47 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// librarian endpoints
+
+// list all titles
 Route::get('/titles/', function () {
-  return Title::all();
+    return Title::all();
+});
+
+// add a new title
+Route::post('/title/add/{isbn}', function ($isbn) {
+    return Title::create($title, $isbn);
+});
+
+// list all overdue books
+Route::get('/copy/overdue', function () {
+    return Copy::overdue();
+});
+
+// add a copy
+Route::post('/copy/add/{isbn}', function ($isbn) {
+    returnCopy::create($isbn);
+});
+
+// delete a copy
+Route::delete('/copy/{sn}', function($sn) {
+    Copy::find($sn)->delete();
+    return 204;
+});
+
+// patron endpoints
+
+// checkout a book
+Route::put('/checkout/{$sn}', function($sn) {
+  return Copy::checkout($sn);
+});
+
+// checkout a book
+Route::delete('/return/{$sn}', function($sn) {
+  return Copy::return($sn);
+});
+
+
+Route::get('/checkouts/', function($id) {
+  return User::checkouts();
 });
