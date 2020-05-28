b17ba1e8160dca963e70e42a5791bc2219cee923
Steve Lefevre <lefevre.10@osu.edu>
2020-05-27 21:55:04 -0400

    Added validate ISBN method to title model

--- app/Title.php
+++ app/Title.php
@@ -9,7 +9,7 @@ class Title extends Model
 
     protected $fillable = ['title','isbn'];
 
-    public function add($isbn, $title) {
+    public static function add($isbn, $title) {
 
         $title = Title::create([
             'isbn' => $isbn,
@@ -19,8 +19,20 @@ class Title extends Model
         return $title->save();
     }
 
-    public function remove($isbn) {
+    public static function remove($isbn) {
         title::where('isbn', $isbn)->delete();
     }
 
+    /**
+     * returns 1 on ISBN-10, 2 on ISBN-13, and FALSE on no valid isbn.
+     */
+    public static function validateIsbn($isbn) {
+
+        $regex = '/\b(?:ISBN(?:: ?| ))?((?:97[89])?\d{9}[\dx])\b/i';
+
+        if ( preg_match($regex, str_replace('-', '', $isbn), $matches)) {
+            return (10 === strlen($matches[1])) ? 1 : 2;
+        }
+        return false;
+    }
 }
