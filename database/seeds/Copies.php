53c629c97ab71178146ad1e0d2c85348a9341dcc
Steve Lefevre <lefevre.10@osu.edu>
2020-05-31 00:49:06 -0400

    Hard-coding checkouts for testing

--- database/seeds/Copies.php
+++ database/seeds/Copies.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 
 class Copies extends Seeder
 {
+
     /**
      * Run the database seeds.
      *
@@ -13,31 +14,46 @@ class Copies extends Seeder
      */
     public function run()
     {
+
+        // user 2 has three copies checked out
+        for ($i = 0; $i < 3; $i++) {
+            $date = date('Y-m-d', strtotime('-'.mt_rand(1,13).' days') );
+            self::add(2, $date, 1234567890 + $i);
+        }
+
+        // user 3 has an overdue book
+        $date = date('Y-m-d', strtotime('-16 days') );
+        self::add(3, $date, 1234567899 );
+
         $faker = \Faker\Factory::create();
 
+        // 30 random copies and checkouts
         for ($i = 0; $i < 30; $i++) {
 
             // make the book checked out 2/3rds of the time
             $user_id = NULL;
             $date = NULL;
             if ( rand(1,10) % 3 > 1 ) {
-              $date = date('Y-m-d', strtotime('-'.mt_rand(1,21).' days') );
-              while ( $user_id === NULL || $user_id == 1 ) {
-                 $user_id = DB::table('users')->inRandomOrder()->first()->id;
-              }
+                 $date = date('Y-m-d', strtotime('-'.mt_rand(1,21).' days') );
+                 while ( $user_id === NULL || $user_id < 4 ) {
+                     $user_id = DB::table('users')->inRandomOrder()->first()->id;
+                 }
             }
-
-            DB::table('copies')->insert([
-                'title_id' => DB::table('titles')->inRandomOrder()->first()->id,
-                'checkout_user_id' => $user_id,
-                'sn' => $faker->ean8(),
-                'acquisition_date' => date('Y-m-d', strtotime('-'.mt_rand(90,7300).' days')),
-                'checkout_date' => $date,
-                'damage_notes' => NULL,
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
-            ]);
+#            self::add($user_id, $date, $faker->ean8());
         }
-
     }
+
+    public function add($user_id, $date, $sn) {
+        DB::table('copies')->insert([
+            'title_id' => DB::table('titles')->inRandomOrder()->first()->id,
+            'checkout_user_id' => $user_id,
+            'sn' => $sn,
+            'acquisition_date' => date('Y-m-d', strtotime('-'.mt_rand(90,7300).' days')),
+            'checkout_date' => $date,
+            'damage_notes' => NULL,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+
 }
--- database/seeds/Titles.php
+++ database/seeds/Titles.php
@@ -13,16 +13,26 @@ class Titles extends Seeder
      */
     public function run()
     {
+        // hard-code this ISBN
+        self::add('War and Peace', '1234567890');
+
         $faker = \Faker\Factory::create();
 
         for ($i = 0; $i < 12; $i++) {
-            DB::table('titles')->insert([
-                'title' => ucwords($faker->sentence($nbWords = rand(1,6), $variableNbWords = true)),
-                'isbn' => $faker->isbn13(),
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            ]);
+            self::add(
+                ucwords($faker->sentence($nbWords = rand(1,6), $variableNbWords = true)),
+                $faker->isbn13()
+            );
         }
 
+    }
+
+    private function add($title, $isbn) {
+        DB::table('titles')->insert([
+            'title' => $title,
+            'isbn' => $isbn,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
     }
 }
